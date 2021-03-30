50877c55a9fa96dd2aa4341ec376fbbd14fb3287
Felipe Risney <41243324+FRisney@users.noreply.github.com>
2021-03-29 22:23:54 -0300

    Update Eleicao.java

--- prog/src/Eleicao.java
+++ prog/src/Eleicao.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Eleicao {
+public class Main {
 
 	public static void main(String[] args) {
 		// Declaracoes
@@ -9,19 +9,18 @@ public class Eleicao {
 			int votos;
 			public Candidato(String nome){ this.nome = nome;}
 		}
-		
-		Candidato candidatos[] = new Candidato[6];
+		Candidato candidatos[] = { 
+			new Candidato("Candidato 1"),
+			new Candidato("Candidato 2"),
+			new Candidato("Candidato 3"),
+			new Candidato("Candidato 4"),
+			new Candidato("Nulo"),
+			new Candidato("Em Branco"),
+		};
+
 		Scanner scan = new Scanner(System.in);
 		int opc = 0, total = 0;
-		
-		// Nomes dos candidatos
-		candidatos[0] = new Candidato("Candidato 1");
-		candidatos[1] = new Candidato("Candidato 2");
-		candidatos[2] = new Candidato("Candidato 3");
-		candidatos[3] = new Candidato("Candidato 4");
-		candidatos[4] = new Candidato("Nulo");
-		candidatos[5] = new Candidato("Em Branco");
-		
+
 		// Entrada de dados
 		while(true) {
 			System.out.print("Candidato > ");
@@ -30,11 +29,10 @@ public class Eleicao {
 			candidatos[opc-1].votos++;
 			total++;
 		}
-		
+
 		// Saida de dados
 		for (int i=0; i< candidatos.length; i++)
 			System.out.println("O " + candidatos[i].nome + " teve " + candidatos[i].votos + " votos");
-		
 		System.out.printf("%.2f%% dos votos foram nulos\n", (float)candidatos[4].votos/total*100);
 		System.out.printf("%.2f%% dos votos foram em branco\n", (float)candidatos[5].votos/total*100);
 		scan.close();
