515b2a72ed4d28d8b658ec68159a94c59731f8c0
Felipe Ferreira <frisney@protonmail.com>
2021-04-12 23:41:32 -0300

    Desenvolvimento da classe Conta e Menu principal

--- ProjetoBanco/src/frisney/com/github/projetobanco/classes/Conta.java
+++ ProjetoBanco/src/frisney/com/github/projetobanco/classes/Conta.java
@@ -1,5 +1,78 @@
 package frisney.com.github.projetobanco.classes;
 
 public class Conta {
+	public boolean log;
+	private String  id;
+	private String  nome;
+	/*
+	 * Saldo em centavos
+	 */
+	private Integer saldo;
+	/**
+	 * @param id
+	 * @param nome
+	 * @param saldo
+	 */
+	public Conta(String id, String nome, Integer saldo) {
+		this.log = true;
+		this.id = id;
+		this.nome = nome;
+		this.saldo = saldo;
+	}
+	/**
+	 * @param id
+	 * @param nome
+	 * @param saldo
+	 */
+	public Conta(String id, String nome, float saldo) {
+		this.log = true;
+		this.id = id;
+		this.nome = nome;
+		this.saldo = (int)saldo*100;
+	}
 
+	public void extrato() {
+		System.out.printf("Na conta %s ha R$%.2f\n", id, (float)saldo/100f);
+	}
+
+	public boolean depositar(float reais) {
+		this.saldo += (int) reais*100;
+		if (log)
+			System.out.printf("Deposito de R$%.2f efetuado com sucesso!\n", reais);
+		return true;
+	}
+
+	public boolean sacar(float reais) {
+		int novo_saldo = this.saldo - (int) reais*100;
+		if (novo_saldo < 0) { 
+			if (log)
+				System.out.println("Sem saldo");
+		 } else {
+			if (log)
+				System.out.println("Saque efetuado com sucesso!");
+			this.saldo = novo_saldo;
+		}
+		extrato();
+		return true;
+	}
+
+	public boolean transferirPara(float reais, Conta destino) {
+		log = false;
+		if (this.id == destino.id) { 
+			System.out.println("Nao e possivel transferir para a sua propria conta!");
+			return false;
+		}
+		if (!sacar(reais)) { 
+			System.out.println("Sem saldo, transferencia nao efetuada");
+			log = true;
+			return false;
+		}
+		destino.log = false;
+		destino.depositar(reais);
+		destino.log = true;
+		log = true;
+		System.out.printf("Transferencia de R$%.2f efetuada com sucesso!\n", reais);
+		return true;
+	}
+	
 }
--- ProjetoBanco/src/frisney/com/github/projetobanco/programa/App.java
+++ ProjetoBanco/src/frisney/com/github/projetobanco/programa/App.java
@@ -1,9 +1,54 @@
 package frisney.com.github.projetobanco.programa;
 
+import java.util.Scanner;
+
+import frisney.com.github.projetobanco.classes.Conta;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		Conta contas[] = {
+				new Conta("001", "Carinha", 10.0f),
+				new Conta("002", "Maluco",  10.0f)
+		};
+		
+		do {
+			float vlr = 0f;
+			System.out.print("digite a conta ");
+			int cnt = scan.nextInt() - 1;
+			if (cnt < 0 || cnt > contas.length)
+				break;
+			System.out.print("digite a operacao ");
+			var opc = scan.nextInt();
+			if (opc == 2 || opc == 3 || opc == 4) {
+				System.out.print("digite o valor ");
+				vlr = scan.nextFloat();
+			}
+			switch(opc) { 
+				case 1:
+					contas[cnt].extrato();
+					break;
+				case 2:
+					contas[cnt].depositar(vlr);
+					break;
+				case 3:
+					contas[cnt].sacar(vlr);
+					break;
+				case 4:
+					System.out.print("digite a conta destino ");
+					var dstn = scan.nextInt() - 1;
+					if (dstn < 0 || dstn > contas.length)
+						System.out.println("Conta destino invalida");
+					else
+						contas[cnt].transferirPara(vlr, contas[dstn]);
+					break;
+				default:
+					System.out.println("Operacao Invalida");
+			}
+		}while(true);
+		System.out.println("Fim");
+		scan.close();
 
 	}
 
